0493a4623323d6edfa5b895ff0a51a024c10c98c
dungvnph06543 <rungruoiit99@gmail.com>
2019-10-17 20:04:20 +0700

    list project

--- src/app/screens/add-project/add-project.component.ts
+++ src/app/screens/add-project/add-project.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CallapiService } from './../../service/callapi.service';
+import {Router, ActivatedRoute, ParamMap} from '@angular/router';
+import {Validators, FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-add-project',
   templateUrl: './add-project.component.html',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProjectComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private apiService : CallapiService,
+    private route : Router
+
+  ) { }
+
+  model = new FormGroup(
+    {
+      name : new FormControl('',[
+       Validators.required,
+       Validators.minLength(4)
+       ]),
+      information : new FormControl('',[
+        Validators.required
+        ])
+    });
 
   ngOnInit() {
   }
+  get name(){ return this.model.get('name');}
+  get information(){ return this.model.get('information');}
 
 }
--- src/app/service/callapi.service.ts
+++ src/app/service/callapi.service.ts
@@ -9,19 +9,22 @@ export class CallapiService {
 
   constructor( private http: HttpClient ) { }
 
-  private ApiUrl='http://localhost:8000/api/admin';
+  private ApiUrl='http://localhost:8000/api';
 
   get()
   {
     return this.http.get(`${this.ApiUrl}/project`);            
   }
-  editProject(id,data)
+  editProject(id, data)
   {
     return this.http.put<any>(`${this.ApiUrl}/project/${id}`, data);
   }
-
+  addProject(data)
+  {
+    return this.http.post<any>(this.ApiUrl,data);
+  }
   getMember()
   {
-    return this.http.get(`${this.ApiUrl}/admin/member`);
+    return this.http.get(`${this.ApiUrl}/member`);
   }
 }
